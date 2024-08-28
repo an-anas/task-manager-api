d9f089c5c06ff54f5c2ee5ab0fbc08989f5c3919
Angelos Anastassi <ang.anastassi@gmail.com>
2024-08-28 13:03:58 +0200

    Initial commit

---
+++ TaskManager.API/TaskManager.API/Models/Task.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TaskManager.API.Models
+{
+    public class Task
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+
+        [BsonElement("title")]
+        public required string Title { get; set; }
+
+        [BsonElement("description")] public string? Description { get; set; }
+
+        [BsonElement("completed")]
+        public bool? Completed { get; set; }
+
+        // Parameterless constructor needed for MongoDB deserialization
+        public Task() { }
+
+        // Constructor for easier instantiation
+        public Task(string id, string title, string? description = null, bool? completed = null)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Completed = completed;
+        }
+    }
+}
